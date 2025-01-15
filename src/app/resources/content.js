ef6a325531d778eec7413c434d20ac8d5935f282
Dhruv Shah <dhruv0310@icloud.com>
2025-01-15 16:45:01 +0530

    Add WGSIP

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -152,7 +152,7 @@ const about = {
 
 const blog = {
     label: 'Blog',
-    title: 'Writing about design and tech...',
+    title: '',
     description: `Read what ${person.name} has been up to recently`
     // Create new blog posts by adding a new .mdx file to app/blog/posts
     // All posts will be listed on the /blog route
